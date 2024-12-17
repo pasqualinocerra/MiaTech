310b2d252b51954442c2062cf2c0fa19e2b29445
pasqualinocerra <pasqualinocerra13@gmail.com>
2024-12-17 17:01:23 +0100

    inserito state

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,77 @@
+import { useState } from "react";
 import CardExercise from "./components/cardExercise"
-
-const imagine = 'https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-const imagine2 = 'https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-const imagine3 = 'https://images.pexels.com/photos/14092973/pexels-photo-14092973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-const imagine4 = 'https://images.pexels.com/photos/17217435/pexels-photo-17217435/free-photo-of-acqua-inverno-montagna-ghiaccio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-const imagine5 = 'https://images.pexels.com/photos/96387/pexels-photo-96387.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-const imagine6 = 'https://images.pexels.com/photos/8905097/pexels-photo-8905097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
-const title = 'Card Uno';
-const title2 = 'Card Due';
-const title3 = 'Card Tre';
-const title4 = 'Card Quattro';
-const title5 = 'Card Cinque';
-const title6 = 'Card Sei';
-const text = 'Questa è la prima card';
-const text2 = 'Questa è la seconda card';
-const text3 = 'Questa è la terza card';
-const text4 = 'Questa è la quarta card';
-const text5 = 'Questa è la quinta card';
-const text6 = 'Questa è la sesta card';
 
 const App = () => {
   
-   return(
-    <>
-    <div className="container">
-      <CardExercise img = {imagine} h1 = {title} p = {text}/>
-      <CardExercise img = {imagine2} h1 = {title2} p = {text2}/>
-      <CardExercise img = {imagine3} h1 = {title3} p = {text3}/>
-      <CardExercise img = {imagine4} h1 = {title4} p = {text4}/>
-      <CardExercise img = {imagine5} h1 = {title5} p = {text5}/>
-      <CardExercise img = {imagine6} h1 = {title6} p = {text6}/>
-    </div>
-    </>
-  )
-}
+  const [cards ,setCard] = useState ([
+    {
+      imagine : 'https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title : 'Card Uno',
+      text : 'Questa è la prima card',
+    },
+    {
+      imagine : 'https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title : 'Card Due',
+      text : 'Questa è la seconda card',
+    },
+    {
+      imagine : 'https://images.pexels.com/photos/14092973/pexels-photo-14092973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title : 'Card Tre',
+      text : 'Questa è la terza card',
+    },
+    {
+      imagine : 'https://images.pexels.com/photos/17217435/pexels-photo-17217435/free-photo-of-acqua-inverno-montagna-ghiaccio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title : 'Card Quattro',
+      text : 'Questa è la quarta card',
+    },
+    {
+      imagine : 'https://images.pexels.com/photos/96387/pexels-photo-96387.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title : 'Card Cinque',
+      text : 'Questa è la quinta card',
+    },
+    {
+      imagine : 'https://images.pexels.com/photos/8905097/pexels-photo-8905097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      title : 'Card Sei',
+      text : 'Questa è la sesta card',
+    }
+  ])
 
-export default App
+  return (
+    <>
+      <div className="container">
+        <CardExercise
+          img={cards[0].imagine}
+          h1={cards[0].title}
+          p={cards[0].text}
+        />
+        <CardExercise
+          img={cards[1].imagine}
+          h1={cards[1].title}
+          p={cards[1].text}
+        />
+        <CardExercise
+          img={cards[2].imagine}
+          h1={cards[2].title}
+          p={cards[2].text}
+        />
+        <CardExercise
+          img={cards[3].imagine}
+          h1={cards[3].title}
+          p={cards[3].text}
+        />
+        <CardExercise
+          img={cards[4].imagine}
+          h1={cards[4].title}
+          p={cards[4].text}
+        />
+        <CardExercise
+          img={cards[5].imagine}
+          h1={cards[5].title}
+          p={cards[5].text}
+        />
+      </div>
+    </>
+  );
+};
+
+export default App;
